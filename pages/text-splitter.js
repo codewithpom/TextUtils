d24cf299c4c09feb48b8d33d7e34faef98429750
Padmashree Jha <codewithpom@gmail.com>
2023-12-22 18:32:59 +0000

    Add meta tags and page title to improve SEO and user experience

--- pages/text-splitter.js
+++ pages/text-splitter.js
@@ -1,6 +1,7 @@
 // This is a Nxt.js page
 // With bootstrap
 import { useState, useEffect } from 'react';
+import Head from 'next/head';
 
 function App() {
     const [text, setText] = useState('');
@@ -15,6 +16,12 @@ function App() {
 
     return (
         <>
+            <Head>
+                <title>Text Splitter</title>
+                <meta name="description" content="Discover the Text Splitter Tool, a powerful solution for efficiently breaking down large volumes of text into manageable segments." />
+                <meta name="keywords" content="text splitter, content organization, writing tool, text segmentation, content creation" />
+                <meta name="author" content="Padmashree Jha" />
+            </Head>
             <div className="container">
                 <div className='text-center'>
                     <h1>Text Splitter</h1>
@@ -88,23 +95,47 @@ function App() {
                     navigator.clipboard.writeText(splittedText);
                     alert('Text copied to clipboard !')
                 }}>Copy</button>
+                {/* class name for full width hr */}
+                <hr
+                    className='text-center'
+                />
+                {/* write about it in at least 1000 words */}
+                <h1 class="mb-4">Introducing the Text Splitter Tool</h1>
 
+                <p class="lead">In the realm of content creation, managing and organizing textual information is a critical aspect of efficiency. Enter the Text Splitter Tool, a powerful solution designed to streamline the process of breaking down large volumes of text into more manageable segments. Let's explore how this tool can revolutionize your content creation workflow.</p>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="mt-4">Key Features:</h2>
+                        <ul>
+                            <li><strong>Effortless Content Division:</strong> The Text Splitter Tool excels in breaking down lengthy textual content into smaller, more digestible sections.</li>
+                            <li><strong>Maintaining Context and Flow:</strong> The segmentation process is intelligently designed to ensure that each split maintains the logical progression of ideas.</li>
+                            <li><strong>Time-Saving Efficiency:</strong> In a world where time is of the essence, the Text Splitter Tool shines as a time-saving asset.</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <h2 class="mt-4">How It Works:</h2>
+                        <ol>
+                            <li><strong>Input Your Content:</strong> Copy and paste your text into the Text Splitter interface.</li>
+                            <li><strong>Adjust Current and New Delimeter:</strong> Customize the segmentation by specifying the desired current and new delimeter.</li>
+                            <li><strong>Generate Segmented Content:</strong> With a simple click, let the Text Splitter work its magic.</li>
+                            <li><strong>Copy, Paste, and Refine:</strong> Easily copy the segmented content and seamlessly integrate it into your writing environment.</li>
+                        </ol>
+                    </div>
+                </div>
+
+                <h2 class="mt-4">Applications:</h2>
+                <ul>
+                    <li><strong>Bloggers and Content Creators:</strong> Simplify the structuring of blog posts and articles for a more engaging reader experience.</li>
+                    <li><strong>Academic Writers:</strong> Facilitate the organization of research papers and essays, ensuring clarity and coherence.</li>
+                    <li><strong>Creative Writers:</strong> Enhance the flow of creative writing projects, from short stories to novels, by breaking them into manageable parts.</li>
+                    <li><strong>Collaborative Writing:</strong> Streamline collaboration among writers by efficiently dividing tasks and content segments.</li>
+                </ul>
+
+                <p class="mt-4">The Text Splitter Tool stands as a testament to the power of innovation in simplifying the intricacies of content creation. Embrace a more efficient and organized writing process with this tool, designed to empower writers across various domains. Try it today and experience a new level of control over your textual creations.</p>
 
             </div>
-            <hr></hr>
-            {/* write about it */}
-            <div className='text-center'>
-                <h2>How to use it ?</h2>
-                <p>Enter the text in the text area and select the existing delimeter and new delimeter and click on the split text button to get the resultant text.</p>
-            </div>
-            <div className='text-center'>
-                <h2>What does it do ?</h2>
-                <p>It splits the text based on the existing delimeter and joins it with the new delimeter.</p>
-            </div>
-            <div className='text-center'>
-                <h2>Why was it made ?</h2>
-                <p>It was made to split the text based on the delimeter and join it with the new delimeter.</p>
-            </div>
+
 
 
             <style jsx>{`
@@ -121,7 +152,7 @@ function App() {
                 h2 {
                     font-size: 2rem;
                     margin-bottom: 1rem;
-                    text-align: center;
+                    
                 }
 
 
