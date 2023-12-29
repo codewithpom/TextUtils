b15b0ce91402297baf3f118688d1005ae61864b9
Padmashree Jha <codewithpom@gmail.com>
2023-12-29 06:42:59 +0000

    Add descriptions to each tool card

--- links.js
+++ links.js
@@ -1,38 +1,45 @@
 let props = {
     'title': 'TxtUtils',
     'pages': [
-
         {
             'text': "Case changer",
             'link': '/case-changer',
+            'description': 'The case changer tool allows you to convert the case of your text into various formats. This includes upper case, lower case, sentence case, and more. It is a handy tool for editing and formatting text documents.'
         },
         {
             'text': 'Reading Time Calculator',
-            'link': '/reading-time-calc'
+            'link': '/reading-time-calc',
+            'description': 'The reading time calculator estimates the time required to read a given piece of text. It uses standard reading speed metrics to provide an accurate estimation. This tool is useful for planning presentations or reading assignments.'
         },
         {
             'text': 'Text Length Calculator',
-            'link': '/text-length-calc'
+            'link': '/text-length-calc',
+            'description': 'The text length calculator counts the number of characters and words in your text. It provides a quick and easy way to check the length of essays, articles, or any other text documents.'
         },
         {
             'text': 'Text Splitter',
-            'link': '/text-splitter'
+            'link': '/text-splitter',
+            'description': 'The text splitter tool breaks down your text into smaller parts based on a specified delimiter. This can be useful for parsing CSV files, splitting text into sentences, or any other situation where you need to divide text.'
         },
         {
             'text': 'Email and URL Extractor',
-            'link': '/extractor'
+            'link': '/extractor',
+            'description': 'The email and URL extractor finds and extracts all email addresses and URLs from your text. This tool is useful for data extraction and can save you time when dealing with large amounts of text.'
         },
         {
             'text': 'Markdown Converter',
-            'link': '/markdown-to-text'
+            'link': '/markdown-to-text',
+            'description': 'The markdown converter tool converts markdown syntax into plain text. It supports all standard markdown features, including headers, lists, and links. This tool is useful for converting markdown documents for non-markdown environments.'
         },
         {
             'text': 'Text to Speech Translator',
-            'link': '/tts'
+            'link': '/tts',
+            'description': 'The text to speech translator converts written text into spoken words. It uses advanced speech synthesis technology to provide high-quality audio. This tool can be useful for accessibility purposes, language learning, or just for fun.'
         },
         {
             'text': 'QR Code Generator',
-            'link': '/qr-generator'
+            'link': '/qr-generator',
+            'description': 'The QR code generator creates a QR code from your input. You can use this tool to generate QR codes for URLs, text, email addresses, phone numbers, and more. QR codes can be scanned with a smartphone to quickly access the encoded information.'
         }
     ]
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,59 @@
-
-
+import Link from "next/link";
+import tools from '../links'
+import SEO from "../components/SEO";
 function App() {
+  // landing page for the app
   return (
     <>
+      <SEO
+        title="Txt Utils"
+        description="The ultimate tool for your text needs. All for free and without any glitches. It is not run by a single person. It is tun by a community of people like you."
+        keywords={[
+          "textutils",
+          "txtutils",
+          "case changer",
+          "text splitter",
+          " text length calculator",
+          " text to speech",
+          " text to speech translator",
+          " text to speech converter",
+        ]}
+      />
+      <div className="container">
+        <div className="text-center">
+          <h1>Txt Utils</h1>
+          <p>
+            The <b>ultimate tool</b> for your text needs. All for <b>free</b>{" "}
+            and <b>without any glitches</b>. It is not run by a single person.
+            It is tun by a <b>community of people</b> like you.
+          </p>
+          <h2>Tools we provide :-</h2>
+          {/* create a group of bootstrap cards for each tool */}
 
+          <div className="row d-flex justify-content-center">
+            {tools['pages'].map((tool) => {
+              return (
+                <div className="col-sm-4 mb-3" key={tool.text}>
+                  <div className="card h-100">
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title">{tool.text}</h5>
+                      <p className="card-text flex-grow-1">
+                        {tool.description}
+                      </p>
 
+                      <Link href={tool.link}>
+                        <button className="btn btn-primary mt-auto">
+                          Try it
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
