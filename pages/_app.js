dd3f781efeea186646b85f005da107296eeaf030
Padmashree Jha <codewithpom@gmail.com>
2024-10-27 22:53:21 +0530

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import "./App.css"
 import Navbar from '../components/Navbar';
 import Head from "next/head";
+import { useEffect } from "react";
 import "./index.css"
 
 // next/script tag
@@ -9,6 +10,14 @@ import Script from 'next/script';
 
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { window.dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'G-NCSN8K5Q01');
+        }
+      }, []);
     return (
         <>
 
@@ -25,6 +34,10 @@ function MyApp({ Component, pageProps }) {
                 <Script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha512-  q5Y1Nrn3HWnEuJ6ujV4T6bX6jww+JjIjzYjJFhF0R+  JGJQ1+  1QvGCnOM9dPxXmjW5f2sO6dJ7P9uYicKxi1ow==" crossOrigin="anonymous"></Script>
                 <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
                 <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossOrigin="anonymous"></Script>
+                <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-NCSN8K5Q01"
+        strategy="afterInteractive"
+      />
             </Head>
 
             
