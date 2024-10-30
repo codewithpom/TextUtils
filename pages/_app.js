dcb8cfec47a08a84a229f6ffa19569ad9797b4fa
Padmashree Jha <codewithpom@gmail.com>
2024-10-30 18:05:40 +0530

    Use nextjs-google-adsense for Google AdSense
    Integrate `nextjs-google-adsense` package to replace direct Google AdSense script tags.
    
    * **package.json**
      - Add `nextjs-google-adsense` to dependencies.
    
    * **pages/_app.js**
      - Import `GoogleAdSense` from `nextjs-google-adsense`.
      - Replace direct Google AdSense script tags with `GoogleAdSense` component.
      - Pass the Google AdSense client ID to the `GoogleAdSense` component.
    
    * **yarn.lock**
      - Add `nextjs-google-adsense` package details.

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import "./index.css"
 
 // next/script tag
 import Script from 'next/script';
+import { GoogleAdSense } from 'nextjs-google-adsense';
 
 
 function MyApp({ Component, pageProps }) {
@@ -20,17 +21,14 @@ function MyApp({ Component, pageProps }) {
       }, []);
     return (
         <>
-
+            
             
             <Head>
             <meta name="google-adsense-account" content="ca-pub-2735251147722172" />
                 <meta name="yandex-verification" content="2fbda7b2014931f2" />
             <meta name="msvalidate.01" content="38099E1AE93A9BC0C89D053978747CF1" />
         
-                <Script
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2735251147722172"
-                    crossOrigin="anonymous"
-                />
+                
                 <Script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha512-  q5Y1Nrn3HWnEuJ6ujV4T6bX6jww+JjIjzYjJFhF0R+  JGJQ1+  1QvGCnOM9dPxXmjW5f2sO6dJ7P9uYicKxi1ow==" crossOrigin="anonymous"></Script>
                 <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
                 <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossOrigin="anonymous"></Script>
@@ -56,6 +54,9 @@ function MyApp({ Component, pageProps }) {
                     `}
                 </Script>
             <Navbar />
+            <GoogleAdSense
+                    publisherId="pub-2735251147722172"
+                />
             <Component {...pageProps} />
         </>
     )
